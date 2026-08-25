6bb9a320f43ff9147d2b5d8d71ca2adf584e2ce8
Lokesh Ghurde <lokeshghurde@gmail.com>
2026-08-26 01:00:35 +0530

    Time: 31 ms (81.96%) | Memory: 82.7 MB (88.94%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        
+        nums.erase(unique(nums.begin(),nums.end()),nums.end());
+
+        return nums.size()!=n;
+    }
+};
